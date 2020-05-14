46fd776248cbee80060b97d6cbb7bb7a0f0d6b39
Papia28 <papianandi769@gmail.com>
2020-05-14 18:34:22 +0530

    Updating

--- src/main/java/webApplication/testingFramework/common/baseFunctions.java
+++ src/main/java/webApplication/testingFramework/common/baseFunctions.java
@@ -1,66 +1,55 @@
 package webApplication.testingFramework.common;
 
 import org.openqa.selenium.WebDriver;
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import webApplication.testingFramework.seleniumBaseFramework.seleniumBase;
 
-
-public final class genericFunctions {
+public final class baseFunctions {
 
 	private static WebDriver driver;
-	private final seleniumBase sb = new seleniumBase();
+	private static final seleniumBase sb = new seleniumBase();
 
-	
-	
-	@Before
-	public void openBrowser()throws Throwable {
-		try { 
-				driver = sb.beforeExecution();
-	}
-		catch(Exception e) {
+	@BeforeSuite
+	public static void openBrowser() throws Throwable {
+		try {
+			driver = sb.beforeExecution();
+		} catch (Exception e) {
 			System.out.println("Error occurred: openBrowser()");
 			e.printStackTrace();
 		}
 	}
-	
-	
-	
-	
-	@After
-	public void closeBrowser() throws Throwable {
+
+	@AfterSuite
+	public static void closeBrowser() throws Throwable {
 		try {
 			sb.afterExecution();
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("Error occurred: closeBrowser()");
 		}
 	}
-	
-	
-	
-	
+
 	// method to launch the URL of the application
-	public void launchBaseURL() throws Throwable {
+	public WebDriver launchBaseURL() throws Throwable {
 		try {
 			String URL = sb.rc.getURL();
 			if (URL != "") {
 				driver.get(URL);
 				Thread.sleep(2000);
 				System.out.println("URL launched successfully.");
-			}
-			else if(URL== "") {
+			} else if (URL == "") {
 				System.out.println("URL cannot be empty!");
 				throw new NullPointerException();
 			}
+			return driver;
 		} catch (Exception e) {
 			System.out.println("Error Occured: launchURL()");
 			e.printStackTrace();
+			return null;
 		}
 	}
 
-	
 	// method to maximize opened browser window
 	public void maximizeBrowser() throws Throwable {
 		try {
--- src/test/java/stepDefinitions/Login.java
+++ src/test/java/stepDefinitions/Login.java
@@ -1,29 +1,30 @@
 package stepDefinitions;
 
-import webApplication.testingFramework.common.genericFunctions;
+import webApplication.testingFramework.common.baseFunctions;
 
-//import org.junit.runner.RunWith;
-//import cucumber.api.junit.Cucumber;
+import org.openqa.selenium.WebDriver;
+
 import io.cucumber.java.PendingException;
 import io.cucumber.java.en.Given;
 
 
-//@RunWith(Cucumber.class)
+
 public final class Login{
 
-	private genericFunctions gf = new genericFunctions();
+	private baseFunctions bf = new baseFunctions();
+	private static WebDriver driver;
 
 	@Given("^application is launched$")
 	public void launchApplication() throws Throwable {
 		try {
-			gf.launchBaseURL();
-			gf.maximizeBrowser();
+			driver = bf.launchBaseURL();
+			bf.maximizeBrowser();
 			Thread.sleep(5000);
 		} catch (PendingException n) {
 			System.out.println("Steps not found.");
 		} catch (Exception e) {
 			e.printStackTrace();
-			System.out.println("Error occurred: Login()");
+			System.out.println("Error occurred: launchApplication()");
 		}
 
 	}
